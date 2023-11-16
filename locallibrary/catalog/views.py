dddda4641be8cd924241c22f7dc7ed780dda34db
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-16 17:28:31 +0700

    Создание представления для регистрации

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import RegistrationForm
+
 
 def home(request):
     return HttpResponse("Hello Group")
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('home')  # Измените на ваше имя для главной страницы
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
