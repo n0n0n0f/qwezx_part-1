9779f1a5a632176322c221a344f3efe7b0fc2ccc
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-16 17:27:09 +0700

    Создал форму регистрации

---
+++ locallibrary/catalog/forms.py
@@ -0,0 +1,20 @@
+# catalog/forms.py
+from django import forms
+from .models import CustomUser
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ['fio', 'email', 'username', 'password', 'confirm_password', 'agreement']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError('Пароли не совпадают.')
