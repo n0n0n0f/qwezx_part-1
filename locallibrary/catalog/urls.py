80311caf834695c572bb52b30f95d2ef1059d591
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-16 17:38:20 +0700

    написал маршруты и представления для входа и выхода

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -12,3 +12,21 @@ urlpatterns = [
     # Другие маршруты...
     path('register/', register, name='register'),
 ]
+
+# catalog/urls.py
+from django.urls import path
+from .views import user_login
+
+urlpatterns = [
+    # Другие маршруты...
+    path('login/', user_login, name='login'),
+]
+
+# catalog/urls.py
+from django.urls import path
+from .views import user_logout
+
+urlpatterns = [
+    # Другие маршруты...
+    path('logout/', user_logout, name='logout'),
+]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -21,3 +21,13 @@ def register(request):
         form = RegistrationForm()
 
     return render(request, 'registration/register.html', {'form': form})
+
+
+# catalog/views.py
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')  # Измените на ваше имя для главной страницы
