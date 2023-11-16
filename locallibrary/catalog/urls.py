334927dd8cd5dc41f06c734a97d6a02b637f67f9
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-16 17:31:47 +0700

    Сделал маршруты для регистрации и входа, а также сделал доп папки для html.registration

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home)
+    path('', views.home)
+]
+
+from django.urls import path
+from .views import register
+
+urlpatterns = [
+    # Другие маршруты...
+    path('register/', register, name='register'),
 ]
