31c22d9fef7b97f5a6ff02fb72c6d077fa4b8a7f
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-16 17:25:40 +0700

    Написал модель для юзера и сделал migrations

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+# catalog/models.py
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    fio = models.CharField(max_length=255, verbose_name='ФИО')
+    email = models.EmailField(unique=True, verbose_name='Email')
+    agreement = models.BooleanField(default=False, verbose_name='Согласие на обработку персональных данных')
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        related_query_name='custom_user',
+        blank=True,
+        verbose_name='Группы',
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_set',
+        related_query_name='custom_user',
+        blank=True,
+        verbose_name='User permissions',
+        help_text='Specific permissions for this user.'
+    )
+
+    def __str__(self):
+        return self.username
