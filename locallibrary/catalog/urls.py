f54cbcd2d7d4af84c55a491cb62b43407dbe1011
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-18 01:12:01 +0700

    Расписал ВСЕ маршруты

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,9 +1,13 @@
+from django.conf import settings
+from django.contrib.auth.views import LogoutView
+from django.template.context_processors import static
+
 from . import views
-from .views import user_login
+from .views import user_login, create_design_request, view_own_requests, delete_design_request, change_status, \
+    add_category, manage_categories
 from .views import register
 from .views import user_logout
 from .views import home
-from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 urlpatterns = [
@@ -11,10 +15,18 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
-    path('', home, name='base_generic'),
     path('home/', home, name='home'),
-    path('logout/', user_logout, name='user_logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('create_design_request/', create_design_request, name='create_design_request'),
+    path('view_own_requests/', view_own_requests, name='view_own_requests'),
+    path('delete_design_request/<int:request_id>/', delete_design_request, name='delete_design_request'),
+    path('change_status/<int:request_id>/<str:new_status>/', change_status, name='change_status'),
+    path('manage_categories/', manage_categories, name='manage_categories'),
+    path('add_category/', add_category, name='add_category'),
+    path('view_own_requests/', view_own_requests, name='view_own_requests'),
 ]
-# Ваш проект/приложение/urls.py
+
+
+
 
 
