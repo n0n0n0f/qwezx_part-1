835978150e2abcd52460c05952ab64bb54d48e92
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-18 01:21:37 +0700

    Ничего такого просто встроенная админка

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,5 +1,6 @@
-# catalog/admin.py
 from django.contrib import admin
+from .models import CustomUser
 from .models import DesignRequest
 
 admin.site.register(DesignRequest)
+admin.site.register(CustomUser)
