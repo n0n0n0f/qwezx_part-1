3bf9389f96fb2d52dde77e038f9523ce105ad409
Mona Lisa <r4sl3xgod@gmail.com>
2023-11-18 20:54:24 +0700

    Импортировал отдельно категорию чтобы создать список

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import CustomUser
 from .models import DesignRequest
+from django.contrib import admin
+from .models import Category
 
 admin.site.register(DesignRequest)
 admin.site.register(CustomUser)
+admin.site.register(Category)
+
+
